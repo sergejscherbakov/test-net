9acf9fbe8611317e5bdd4ab39204c34ad2743041
Лиза <Liza_Pazhukova@mail.ru>
2015-10-06 20:53:57 +0300

    dz

--- in.js
+++ in.js
@@ -1,3 +1,10 @@
-/* тестовый проект jscourses Киев 2015
- */
-// функция: Лиза, юниты: Сергей
+function divisibleBy(numbers, divisor){
+    var arr = [];
+    for(var i = 0; i < numbers.length; i++){
+
+        if ((numbers[i]%divisor)===0){
+            arr.push(numbers[i]);
+        }
+    }
+    return arr;
+}
